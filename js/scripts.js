30d817f8b90f6afc7156e56f296e8c346876a52d
Molly LeCompte and Ryan Vinyard <ryan@>
2016-08-11 13:22:09 -0700

    add jquery to make a selector box for critter picking

--- js/scripts.js
+++ js/scripts.js
@@ -20,3 +20,49 @@ $(document).ready(function () {
   event.preventDefault();
   });
 });
+
+$(document).ready(function () {
+  $("form#critterselector").submit(function(event) {
+    var age = parseInt($("input#age").val());
+    var creepiness = $("select#creepiness").val();
+
+    if (age <= 16 && creepiness === "tame") {
+      $("#randallturtle").show();
+      $("#larryworm, #severussnake, #mortonmoth, #luciferturtle, #dingusviper, #brucethegoose, #beardodragon, #terrytarantula").hide();
+    }
+    else if (age <= 16 && creepiness === "moderate") {
+      $("#larryworm").show();
+      $("#randallturtle, #severussnake, #mortonmoth, #luciferturtle, #dingusviper, #brucethegoose, #beardodragon, #terrytarantula").hide();
+    }
+    else if (age <= 16 && creepiness === "creepyaf") {
+      $("#severussnake").show();
+      $("#randallturtle, #larryworm, #mortonmoth, #luciferturtle, #dingusviper, #brucethegoose, #beardodragon, #terrytarantula").hide();
+    }
+    else if (age > 16 && age < 65 && creepiness === "tame") {
+      $("#mortonmoth").show();
+      $("#randallturtle, #larryworm, #severussnake, #luciferturtle, #dingusviper, #brucethegoose, #beardodragon, #terrytarantula").hide();
+    }
+    else if (age > 16 && age < 65 && creepiness === "moderate") {
+      $("#luciferturtle").show();
+      $("#randallturtle, #larryworm, #severussnake, #mortonmoth, #dingusviper, #brucethegoose, #beardodragon, #terrytarantula").hide();
+    }
+    else if (age > 16 && age < 65 && creepiness === "creepyaf") {
+      $("#dingusviper").show();
+      $("#randallturtle, #larryworm, #severussnake, #mortonmoth, #luciferturtle, #brucethegoose, #beardodragon, #terrytarantula").hide();
+    }
+    else if (age >= 65 && creepiness === "tame") {
+      $("#brucethegoose").show();
+      $("#randallturtle, #larryworm, #severussnake, #mortonmoth, #luciferturtle, #dingusviper, #beardodragon, #terrytarantula").hide();
+    }
+    else if (age >= 65 && creepiness === "moderate") {
+      $("#beardodragon").show();
+      $("#randallturtle, #larryworm, #severussnake, #mortonmoth, #luciferturtle, #dingusviper, #beardodragon, #terrytarantula").hide();
+    }
+    else if (age >= 65 && creepiness === "creepyaf") {
+      $("#terrytarantula").show();
+      $("#randallturtle, #larryworm, #severussnake, #mortonmoth, #luciferturtle, #dingusviper, #brucethegoose, #beardodragon").hide();
+      }
+
+  event.preventDefault();
+  });
+    });
